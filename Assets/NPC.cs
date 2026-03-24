a27d50546054161c004b2221698fc0271c8b2751
karolina! <karolinakrz098@gmail.com>
2026-03-24 12:59:08 +0000

    More decor, NPC Dialogue system
    added more decoration to enhance game feel, added npc dialogue, jsut need to make npc interactable

---
+++ Assets/NPC.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour, IInteractable
+{
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    public void Interact()
+    {
+        if(dialogueData == null)
+        return;
+
+        if(isDialogueActive)
+        {
+            NextLine();
+        } 
+        else
+        {
+            StartDialogue();
+        }
+    }
+    
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.name);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+        //pause controller, player cant move while npc talk
+
+        StartCoroutine(TypeLine());
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            //skip typing anim and show full line
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            //if another line, type next line
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+        //pause controller = false
+    }
+}
