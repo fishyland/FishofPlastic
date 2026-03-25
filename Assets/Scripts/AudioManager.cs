6f770098be6d878c2e4f3ca7a68a8615676881ad
karolina! <karolinakrz098@gmail.com>
2026-03-25 12:03:54 +0000

    Added interaction UI, Updated AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,83 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance {get; private set;}
+   private static AudioManager Instance;
+   private static AudioSource audioSource;
+   private static SoundEffectLibrary soundEffectLibrary;
+   [SerializeField] private Slider sfxSlider;
 
-    [Header("Audio Sources")]
-    public AudioSource musicSource;
-    public AudioSource sfxSource;
-    public AudioSource audioSource;
-    
-    
-    [Header("AudioClips")]
-    public AudioClip backgroundMusic;
-    public AudioClip kickSFX;
-    public AudioClip damageSFX;
-    public AudioClip bossMusic, defaultMusic;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+   private void Awake()
     {
         if(Instance == null)
         {
             Instance = this;
+            audioSource = GetComponent<AudioSource>();
+            soundEffectLibrary = GetComponent<SoundEffectLibrary>();
             DontDestroyOnLoad(gameObject);
-
-        } 
+        }
         else
         {
             Destroy(gameObject);
         }
-        }
+    }
     
-   private void Start()
+    public static void Play(string soundName)
     {
-        audioSource = GetComponent<AudioSource>();
-        defaultMusic = audioSource.clip;
-
-        if (backgroundMusic != null && musicSource != null)
+        AudioClip audioClip = soundEffectLibrary.GetRandomClip(soundName);
+        if(audioClip != null)
         {
-            musicSource.clip = backgroundMusic;
-            musicSource.loop = true;
-            musicSource.Play();
-        }
-    }
-    public void PlaySFX(AudioClip clip)
-    {
-        if(clip!=null && sfxSource != null)
-        {
-            sfxSource.PlayOneShot(clip);
-        }
-    }
-    public void PlayMusic(AudioClip clip)
-    {
-        if(clip != null && musicSource != null)
-        {
-            musicSource.clip = clip;
-            musicSource.loop = true;
-            musicSource.Play();
+            audioSource.PlayOneShot(audioClip);
         }
     }
 
-     private void OnTriggerEnter2D(Collider2D collision)
+    void Start()
     {
-        if(collision.gameObject.tag == "Player")
-        {
-            audioSource.clip = bossMusic;
-            audioSource.Play();
-        }
+        sfxSlider.onValueChanged.AddListener(delegate { OnValueChanged(); });
     }
-    
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-          if(collision.gameObject.tag == "Player")
-        {
-            audioSource.clip = defaultMusic;
-            audioSource.Play();
-        }
-    }
-    
 
+    public static void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+    public void OnValueChanged()
+    {
+        SetVolume(sfxSlider.value);
+    }
 }
 
