fdd844fd3d373c0dc2a09223310cedb3ee3413e7
karolina! <karolinakrz098@gmail.com>
2026-04-15 15:11:53 +0100

    Making outside pretty

--- Assets/Scripts/EnemyScripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyScripts/EnemyCombat.cs
@@ -5,6 +5,7 @@ public class EnemyCombat : MonoBehaviour
     [SerializeField] private Transform attackPoint;
 
     private EnemyConfig config;
+    public Collider2D hit;
     private Enemy enemy;
     //private PlayerHealth currentHealth;
     private float lastAttackTime;
@@ -13,6 +14,7 @@ private void Start()
     {
         enemy = GetComponent<Enemy>();
         config = enemy.Config;
+        hit = GetComponent<BoxCollider2D>();
        // currentHealth = GetComponent<PlayerHealth>();
     }
 
@@ -31,5 +33,5 @@ public void PerformMeleeAttack()
         if(currentHealth != null)
         currentHealth.ApplyDamage(-config.meleeDamage);
     }
-
+    
 }
